7743ec5b2daa5cf82ef04a192310cf04805df440
ChamlingUttam <uttamchamling51@gmail.com>
2026-02-14 11:41:24 +0545

    setup frontend and pages and created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Page from './Page'
+import Register from './pages/Register'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Page/> */}
+      <Register/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white rounded-2xl shadow-lg max-w-md w-full p-8">
+        <h1 className="text-2xl font-bold text-center text-red-500">
+          Register
+        </h1>
+
+        <form className="space-y-5 mt-6">
+          <div>
+            <label className="block text-sm font-medium mb-2">
+              Full Name
+            </label>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-red-500 text-white py-2 rounded-xl hover:bg-red-600 transition"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
